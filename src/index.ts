a3e6ecc537f6ed18f487180cd4159e525c3bd556
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-04-22 23:22:26 -0400

    - - -

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+
+
+const __filename = fileURLToPath( import.meta.url )
+const __dirname = path.dirname( __filename )
+
+
+for(const filename of fs.readdirSync( __dirname )){
+    
+    console.log( filename )
+
+}
