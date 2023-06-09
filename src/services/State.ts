ccf1161ec2b80cc4e38154eaecd8fcc41aac183b
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-06-09 07:09:46 -0400

    ---

--- src/services/State.ts
+++ src/services/State.ts
@@ -131,6 +131,7 @@ export default class State<S = any> extends Function {
 		this.emit('update', state)
 	}
 
+
 	private __call() {
 		const [state, setState] = React.useState(this.state)
 		React.useEffect(() =>
